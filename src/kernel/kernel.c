420b5d886b663c8e360763457a7bc5d40413c380
marat <comaevmarat4@gmail.com>
2026-09-17 19:20:19 +0300

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -7,13 +7,8 @@ void kmain(void) {
     const static char s[] = "hello to my 32 bit os)))";
     const static char enter[] = " \n";
     
-    // Чистый, красивый порядок вызовов
     clear();
     print(s);
     print(enter);
 
-    init_idt(); // Настраиваем и загружаем IDT
-    
-    // СТРОЧКУ __asm__ volatile("sti"); МЫ ОТСЮДА УДАЛИЛИ!
-    // Функция kmain завершается и возвращает управление в entry.asm
 }
